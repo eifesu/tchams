ee8754c6f75c31a9deacee6af58a9beb1c849ee5
eifesu <yessochrisa@gmail.com>
2023-02-02 16:27:18 +0000

    Added an adapter or whatever

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+// @ts-nocheck
+import netlify from "solid-start-netlify"
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({
+    adapter: netlify({edge: true})
+  })],
 });
